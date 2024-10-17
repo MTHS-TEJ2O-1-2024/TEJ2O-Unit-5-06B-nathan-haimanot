779ee61bfd273076cba894638f105f3b61ee51ed
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-17 19:25:45 -0400

    made a distance sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
+
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Nathan Haimanot
+ * Created on: Oct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//find distance from sonar
+input.onButtonPressed(Button.A, function (){
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
